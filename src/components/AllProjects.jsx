9f9fadefad2a80ce23443cbe1d79d08a12010288
unknown <noman40046@gmail.com>
2025-02-25 21:37:23 +0600

    project done

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -12,23 +12,104 @@ const Allprojects = () => {
               </div>
 
               <div className="mx-auto max-w-7xl px-6 lg:px-8">      
-                <div className="grid grid-cols-1 mt-12 gap-0 px-5 lg:mt-12 md:grid-cols-2">
+                <div className="grid grid-cols-1 mt-12 gap-x-10 gap-y-14 px-5 lg:mt-12 md:grid-cols-2">
                   
-                  <div className="md:px-4 lg:px-10">
-                    <div className="w-full max-w-md mx-auto"> 
-                      <img className="rounded-lg" src="../../public/pro-1.png" alt="" />
-                     <h1 className="text-2xl font-bold pt-5 pb-3 hover:text-[#FF014F]">Consulting and Advisory Firm</h1>
+                  
+                    <div className="w-[95%] max-w-md mx-auto"> 
+                      <img className="rounded-2xl border border-gray-700 transition-transform duration-300 hover:scale-105" src="../../project-1.jpg" alt="" />
+                     <h1 className="text-2xl font-bold pt-5 pb-3 hover:text-[#FF014F] transition-all duration-1000">Consulting and Advisory Firm</h1>
                      <a target="_blank"
                     href="https://sparkaccountingpartners.com/"
                     title=""
-                    className="border border-[#FF014F] inline-flex items-center justify-center px-5 py-2 text-md font-semibold text-white transition-all duration-200  rounded-lg hover:bg-white hover:text-[#FF014F] hover:border-[white]"
+                    className="border border-[#FF014F] inline-flex items-center justify-center px-5 py-2 text-md font-semibold text-white transition-all duration-500  rounded-lg hover:bg-white hover:text-[#FF014F] hover:border-[white] "
                     role="button"
                   >                 
                     Visit Here
                   </a>
                     </div>
-                  </div>
-                  
+
+                    <div className="w-[95%] max-w-md mx-auto"> 
+                      <img className="rounded-2xl border border-gray-700 transition-transform duration-300 hover:scale-105" src="../../project-2.jpg" alt="" />
+                     <h1 className="text-2xl font-bold pt-5 pb-3 hover:text-[#FF014F] transition-all duration-1000">Energy Solutions Company</h1>
+                     <a target="_blank"
+                    href="https://thunderbox.webgive.net/"
+                    title=""
+                    className="border border-[#FF014F] inline-flex items-center justify-center px-5 py-2 text-md font-semibold text-white transition-all duration-500  rounded-lg hover:bg-white hover:text-[#FF014F] hover:border-[white]"
+                    role="button"
+                  >                 
+                    Visit Here
+                  </a>
+                    </div>
+
+                    <div className="w-[95%] max-w-md mx-auto"> 
+                      <img className="rounded-2xl border border-gray-700 transition-transform duration-300 hover:scale-105" src="../../project-3.jpg" alt="" />
+                     <h1 className="text-2xl font-bold pt-5 pb-3 hover:text-[#FF014F] transition-all duration-1000">Neighborhood Revitalization Firm</h1>
+                     <a target="_blank"
+                    href="https://theopportunityzo-vcnj7bqfb6.live-website.com/"
+                    title=""
+                    className="border border-[#FF014F] inline-flex items-center justify-center px-5 py-2 text-md font-semibold text-white transition-all duration-500  rounded-lg hover:bg-white hover:text-[#FF014F] hover:border-[white]"
+                    role="button"
+                  >                 
+                    Visit Here
+                  </a>
+                    </div>
+
+
+                    <div className="w-[95%] max-w-md mx-auto"> 
+                      <img className="rounded-2xl border border-gray-700 transition-transform duration-300 hover:scale-105" src="../../project-4.jpg" alt="" />
+                     <h1 className="text-2xl font-bold pt-5 pb-3 hover:text-[#FF014F] transition-all duration-1000">Online Travel Agency</h1>
+                     <a target="_blank"
+                    href="https://flightbookinglobal.com/"
+                    title=""
+                    className="border border-[#FF014F] inline-flex items-center justify-center px-5 py-2 text-md font-semibold text-white transition-all duration-500  rounded-lg hover:bg-white hover:text-[#FF014F] hover:border-[white]"
+                    role="button"
+                  >                 
+                    Visit Here
+                  </a>
+                    </div>
+
+
+                    <div className="w-[95%] max-w-md mx-auto"> 
+                      <img className="rounded-2xl border border-gray-700 transition-transform duration-300 hover:scale-105" src="../../project-5.jpg" alt="" />
+                     <h1 className="text-2xl font-bold pt-5 pb-3 hover:text-[#FF014F] transition-all duration-1000">Qur'an Academy & Hifz Madrasa</h1>
+                     <a target="_blank"
+                    href="https://tr.webgive.net/"
+                    title=""
+                    className="border border-[#FF014F] inline-flex items-center justify-center px-5 py-2 text-md font-semibold text-white transition-all duration-500  rounded-lg hover:bg-white hover:text-[#FF014F] hover:border-[white]"
+                    role="button"
+                  >                 
+                    Visit Here
+                  </a>
+                    </div>
+
+
+                    <div className="w-[95%] max-w-md mx-auto"> 
+                      <img className="rounded-2xl border border-gray-700 transition-transform duration-300 hover:scale-105" src="../../project-6.jpg" alt="" />
+                     <h1 className="text-2xl font-bold pt-5 pb-3 hover:text-[#FF014F] transition-all duration-1000">Technical and Technology Experts</h1>
+                     <a target="_blank"
+                    href="https://guide-of-future.netlify.app/"
+                    title=""
+                    className="border border-[#FF014F] inline-flex items-center justify-center px-5 py-2 text-md font-semibold text-white transition-all duration-500  rounded-lg hover:bg-white hover:text-[#FF014F] hover:border-[white]"
+                    role="button"
+                  >                 
+                    Visit Here
+                  </a>
+                    </div>
+
+
+                    <div className="w-[95%] max-w-md mx-auto"> 
+                      <img className="rounded-2xl border border-gray-700 transition-transform duration-300 hover:scale-105" src="../../project-7.jpg" alt="" />
+                     <h1 className="text-2xl font-bold pt-5 pb-3 hover:text-[#FF014F] transition-all duration-1000">QR Code Generator App</h1>
+                     <a target="_blank"
+                    href="https://qc-generator.netlify.app/"
+                    title=""
+                    className="border border-[#FF014F] inline-flex items-center justify-center px-5 py-2 text-md font-semibold text-white transition-all duration-500  rounded-lg hover:bg-white hover:text-[#FF014F] hover:border-[white]"
+                    role="button"
+                  >                 
+                    Visit Here
+                  </a>
+                    </div>
+
                 </div>
               </div>
 
